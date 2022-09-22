ab89894dcfc1f70cf3cf99db52f81265d0de350a
CandelaLucena <candelalucenareyes1212@gmail.com>
2022-09-22 13:54:16 +0200

    Javascript

--- codigo4.js
+++ codigo4.js
@@ -1,9 +1,10 @@
 //Candela Lucena Reyes
 function fechaActual(){
+    var fecha = new Date();
     var hoy = fecha.getDate();
     var mesActual = fecha.getMonth() + 1; 
     var anyoActual = fecha.getFullYear();
-    var dia = Date.prototype.getDay();
+    var dia = fecha.getDay();
     
     document.write(hoy+" "+mesActual+" "+anyoActual);
 }
