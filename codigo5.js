057722fb5e96c754faf59d56c54c36f93222c06b
CandelaLucena <candelalucenareyes1212@gmail.com>
2022-09-22 13:40:26 +0200

    JavaScript

--- codigo5.js
+++ codigo5.js
@@ -1,7 +1,2 @@
 //Candela Lucena Reyes
 
-var precio = parseInt (prompt("Dime un precio",""));
-var calculoIva = precio * 0.21;
-var precioTotal = precio + calculoIva;
-
-alert(precioTotal.toFixed(2));
