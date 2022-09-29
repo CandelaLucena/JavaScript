4680853e7614e4305a4403406643bb7d5ca16b0d
CandelaLucena <candelalucenareyes1212@gmail.com>
2022-09-29 13:52:14 +0200

    ejercicios javascript

---
+++ anexocodigo2.js
@@ -0,0 +1,19 @@
+//Candela Lucena
+
+function formularioVacio() {
+    
+    //Obtener el DNI puesto
+    var numerodeDNI = document.getElementById("dni").value;
+    //Comprobar si el DNI esta vacio
+    if (numerodeDNI.length == 0) {   
+        //if(numero="")
+        mensaje = "No has introducido un DNI"
+    }else{
+        mensaje = "DNI detectado";
+        document.getElementById("dni").submit;
+    }
+    
+    //Enviar si el DNI esta vacio o no
+    document.getElementById("texto").innerHTML = mensaje;
+   
+}
