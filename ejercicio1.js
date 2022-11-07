23725798ad84f14d5bca5d445b0121b073899b3f
CandelaLucena <candelalucenareyes1212@gmail.com>
2022-11-07 12:12:31 +0100

    javascript

--- ejercicio1.js
+++ ejercicio1.js
@@ -6,7 +6,14 @@ window.onload = function(){
 
   var formulario = document.getElementById("formulario");
   formulario.addEventListener("submit", comprobar);
+
+  var reseteo = document.getElementById("botonReseteo");
+  reseteo.addEventListener("click", resetear);
+
+  var texto = document.getElementById("parrafo");
+  texto.addEventListener("keydown",limite)
 }
+
 function listar(event){
   //longitud de la lista,número del índice seleccionado y el valor del índice
   var lista = document.getElementsByTagName("option");
@@ -20,10 +27,26 @@ function comprobar(event){
   //DNI obligatorio y Teléfono debe estar formado por nueve dígitos consecutivos y sin espacios ni guiones entre las cifras.
   var input1 = document.getElementById("integerTelefono");
   var input2 = document.getElementById("integerDni");
-  if(input1.value="^[1-9]\d{9}\." || input2.value==""){
+  //comprobar si la expresion esta bien o mal
+  expresion = new RegExp("^[1-9]\d{9}\.");
+  if(input1.value.match(expresion) || input2.value==""){
     alert("No envió");
     event.preventDefault();
   }else{
     alert("Envió");
+  }
+}
+
+function resetear(event){
+  //resetea el formulario, necesita getelementbyid del formulario
+  document.getElementById("formulario").reset();
+}
+
+function limite(event){
+  //comprueba el limite de caracteres
+  if((this.value.length)<151 || event.keyCode==8 || event.keyCode==46){
+
+  }else{
+    event.preventDefault();
   }
 }
